3800328c45c6002f7d2ce3c51e05dc0a9f4a0708
Jayakrishna14s <krishna9502202491@gmail.com>
2026-02-09 06:44:17 +0000

    jk-3

---
+++ doubtconnect/src/main/java/com/app/doubtconnect/DoubtConnectApplication.java
@@ -0,0 +1,13 @@
+package com.app.doubtconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubtConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubtConnectApplication.class, args);
+	}
+
+}
