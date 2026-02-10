d5be6a81ca7470f548455e0a310185c4e7f7f646
Jayakrishna14s <krishna9502202491@gmail.com>
2026-02-11 01:00:54 +0530

    frontend init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile";  
-import Search from "./pages/Search";
-import HomePage from "./pages/HomePage"; 
-
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />  
-        <Route path="/search" element={<Search />} />
-        <Route path="/homepage" element={<HomePage />} /> 
-
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const App = () => {
+  return <div className="text-red-500">App</div>;
+};
 
 export default App;
